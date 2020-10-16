f9179f0f29fd900ed913ef478df1f3d88c9a7e25
Søren Guldmund <sgu@immeo.dk>
2020-10-16 09:53:33 +0200

    Add Capture class

---
+++ FakeItEasy.Capture.Sandbox/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FakeItEasy.Capture.Sandbox
+{
+    public interface Test
+    {
+        bool X(string str);
+    }
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var test = A.Fake<Test>();
+            var stringCapture = new Capture<string>();
+            A.CallTo(() => test.X(stringCapture)).Returns(true);
+
+            var b = test.X("Hello, World");
+            var b1 = test.X(", World");
+            
+            Console.WriteLine(b);
+            foreach (var v in stringCapture.Values)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
